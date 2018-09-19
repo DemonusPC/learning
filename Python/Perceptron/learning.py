917544699d60975d30c96b0dace08cf2808d849b
Andrzej Pogonowski <pogon.ap@hotmail.com>
2018-09-19 11:40:12 +0100

    Added graphing

--- Python/Perceptron/learning.py
+++ Python/Perceptron/learning.py
@@ -46,7 +46,7 @@ not_output = [1,0]
 # 1.2 3.0 1 1
 # 7.8 6.1 1 -1
 s_data = [[1.0, 1.0, 1.0], [9.4,6.4,1.0], [2.5,2.1,1.0], [8.0,7.7,1.0], [0.5,2.2,1.0], [7.9, 8.4, 1.0], [7.0, 7.0, 1.0] , [2.8,0.8,1.0],[1.2,3.0,1.0],[7.8,6.1,1.0]]
-s_output = [1,-1,1,-1,1,-1,1,-1,-1,1,1,-1]
+s_output = [1,0,1,0,1,0,1,0,0,1,1,0]
 
 plot_data_x = []
 plot_data_y = []
@@ -55,73 +55,131 @@ for point in s_data:
     plot_data_x.append(point[0])
     plot_data_y.append(point[1])
 
+fig = plt.figure(figsize=(7,10))
+learning_graph = fig.add_subplot(211)
+learning_graph.set_title("Data and learning")
+error_graph = fig.add_subplot(212)
+error_graph.set_title("Global Error")
+
+
+
+error_list = [20]
+global_error_list = [20]
+error_graph.plot(error_list)
+error_graph.plot(global_error_list)
+
+
 line = np.linspace(9.5,0,10)
-line = np.linspace(-1.13,)
 
-plt.scatter(plot_data_x, plot_data_y)
-plt.plot(line)
-plt.axis([0.0, 10.0, 0.0, 10.0])
-plt.xlabel('x1')
-plt.ylabel('x2')
+def plot_standard():
+    learning_graph.scatter(plot_data_x, plot_data_y)
+    learning_graph.plot(line)
+
+# learning_graph.scatter(plot_data_x, plot_data_y)
+# learning_graph.plot(line)
+plot_standard()
+error_graph.set_ylim([0,20])
+learning_graph.axis([0.0, 10.0, 0.0, 10.0])
+
+
+
+plt.ion()
 plt.show()
-
-
 
 epoch_target = 5
 
 
-def learning(inputs, outputs):
-    p = perceptron.Perceptron(perceptron.x_simple) 
+def learning(inputs, outputs, function):
+    p = perceptron.Perceptron(function) 
     learning_rate = 0.2
-    err = 100
-    err_flag = False
-    epoch = 0
+    global_error = 20
+    previous_weights = [p.get_weights()]
 
-    while err != 0 or epoch < epoch_target:
-        i = 0
+    iteration = 0
+
+    while global_error != 0:
         results = []
+        previous_local_error = 20
+        i = 0
 
         for input in inputs:
-            index = randint(0, len(inputs)-1)
-            result = p.run(inputs[index])
+            inp,o = generate_input_pair(inputs, outputs)
+            result = p.run(inp)
             results.append(result)
-            error = round(outputs[i] - result, 2)
+            local_error = o - result
             pW = ""
             # print(p.get_weights())
-            if result != outputs[i]:
+            if result != o:
                 # deltaWi = a * ( y - h ) * xi
                 # a = learning rate
                 # y is the desired output
                 # h is the actual output
                 # xi is the input
-                new_w = [round(y +round((learning_rate * x * error), 2), 2) for x,y in zip(inputs[index], p.get_weights())]
+                new_w = [round(y +round((learning_rate * x * local_error), 3), 3) for x,y in zip(inp, p.get_weights())]
                 # print(new_w)
                 pW = new_w
                 p.setWeights(new_w)
-                err_flag = True
-            printRound(input, result, outputs[i], pW)
+                previous_local_error = local_error
+            printRound(input, result, o, pW)
             i += 1
 
 
-        err = calculate_error(outputs, results)
-        if err_flag == False:
-            epoch += 1
-            learning_rate -= 0.05
-            print("raising epoch")
-        else:
-            print("fail")
-            err_flag = False
-            epoch = 0
-            learning_rate = 0.2
+        overall_error = calculate_error(outputs, results)
+
+        if(overall_error < global_error):
+            if round(learning_rate,1) > 0.06:
+                learning_rate -= 0.01
+                learning_rate = round(learning_rate, 3)
+            global_error = overall_error
+
+
+        print(learning_rate)
+
+        # global_error = overall_error
+
+        error_list.append(overall_error)
+        global_error_list.append(global_error)
+
+        if len(error_list) > 20:
+            error_list.pop(0)
+
+        if len(global_error_list) > 20:
+            global_error_list.pop(0)
+
+
+        # if err_flag == False:
+        #     epoch += 1
+        #     learning_rate -= 0.02
+        #     print("raising epoch")
+        # else:
+        #     print("fail")
+        #     err_flag = False
+        #     epoch = 0
+        #     learning_rate = 0.1
         
-        print(err)
-        print(epoch)
-        print(epoch_target)
-        if epoch < epoch_target:
-            print("Should not continue")
-        
-        if epoch < epoch_target or err != 0:
-            print("Should not continue")
+        weights = p.get_weights()
+        m = calculate_gradient(weights[1], weights[0], weights[2])
+        w, q = produce_points(m, weights[2])
+        line = np.linspace(w,q)
+
+        learning_graph.clear()
+        plot_standard()
+        learning_graph.plot(line)
+        learning_graph.set_xlim([0,10])
+        learning_graph.set_ylim([0,10])
+
+        error_graph.clear()
+        error_graph.plot(error_list)
+        error_graph.plot(global_error_list)
+        error_graph.set_ylim([0,20])
+
+        plt.draw()
+        plt.pause(0.01)
+
+        iteration += 1
+        print(global_error)
+        print(iteration)
+
         
 
     print("Learning completed!")
@@ -139,10 +197,36 @@ def calculate_error(desired, output):
     error = 0.5 * sum([(x-y)**2 for x, y in zip(desired, output)])
     return error
 
-t = learning(s_data,s_output)
-# t = learning(or_data,or_output)
+def generate_input_pair(inputs, outputs):
+    index = randint(0, len(inputs)-1)
+    input = inputs[index]
+    output = outputs[index]
+    return input,output
+
+
+# linear calculation
+# y, x, c
+def calculate_gradient(x2, x1, bias):
+    if x1 == 0:
+        return x2-bias
+    m = (x2-bias) / x1
+    return m
+
+def produce_points(gradient, bias):
+    # y = mx+c
+    first = bias
+    second = (10 * gradient) + bias
+    
+    return first,second
+
+
+
+# t = learning(s_data,s_output, perceptron.sigmoid)
+# t = learning(or_data,or_output, perceptron.simple)
 # t = learning(not_data,not_output)
-p2 = perceptron.Perceptron(perceptron.x_simple, t)
+# p2 = perceptron.Perceptron(perceptron.sigmoid, t)
+
+p2 = perceptron.Perceptron(perceptron.sigmoid, [-1.13, -1.1, 10.9])
 
 # print(str(p2.run([0,0,1])) + " should 0")
 # print(str(p2.run([0,1,1])) + " should 0")
@@ -156,7 +240,22 @@ p2 = perceptron.Perceptron(perceptron.x_simple, t)
 
 # print(str(p2.run([0,1])))
 
-print(str(p2.run([8.3,9.2,1])) + " should -1")
+# print(str(p2.run([8.3,9.2,1])) + " should -1")
+
+print(p2.get_weights())
+
+
+
+correct = 0
+
+for inp, exp in zip(s_data, s_output):
+    result = p2.run(inp)
+    if(result == exp):
+        correct += 1
+    print(str(result) + " = " + str(exp))
+
+accuracy = (correct / len(s_output)) * 100
+print("Accuracy: " + str(accuracy))
 
 
 
--- Python/Perceptron/perceptron.py
+++ Python/Perceptron/perceptron.py
@@ -37,10 +37,10 @@ def acti(x, y):
 def sigmoid(x):
     if x >= 0:
         z = math.exp(-x)
-        return 1 / (1 + z)
+        return round(1 / (1 + z),1)
     else:
         z = math.exp(x)
-        return z / (1 + z)
+        return round(z / (1 + z),1)
 
 def sigmoidDerivative(x):
     return sigmoid(1-sigmoid(x))
@@ -52,7 +52,7 @@ def simple(x):
         return 0
 
 def x_simple(x):
-    if x >= 5:
+    if x >= 5.0:
         return 1
     else:
         return -1
